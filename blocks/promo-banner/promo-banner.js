11a57e01fb8de15ec64e8a23ba9350c3cc6d2a7e
gbedekar489 <161075939+gbedekar489@users.noreply.github.com>
2025-08-14 02:12:26 -0700

    Update promo-banner.js

--- blocks/promo-banner/promo-banner.js
+++ blocks/promo-banner/promo-banner.js
@@ -1,1 +1,10 @@
+// blocks/promo-banner/promo-banner.js
+export default function decorate(block) {
+  block.innerHTML = `
+    <div class="promo-content">
+      <h2>Special Offer!</h2>
+      <p>Sign up today and save 20%</p>
+    </div>
+  `;
+}
 
